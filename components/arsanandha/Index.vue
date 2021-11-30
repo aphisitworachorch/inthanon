4421184fa00096abedef2cbe9ccbf99296e90077
Arsanandha Aphisitworachorch <arsanandha.ap@gmail.com>
2021-11-30 13:23:43 +0700

    Initial Inthanon

---
+++ components/arsanandha/Index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="centerLogo"  @mouseover="showText = true" @mouseleave="showText = false">
+    <div>
+      <img :src="logo" alt="ARSANANDHA.xyz Logo"/>
+    </div>
+    <p v-if="showText">Coming soon ... (arsanandha.xyz)</p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Index",
+  data: function () {
+    return {
+      logo: require("../logo/arsanandha.xyz-v2-01.png"),
+      showText: false
+    }
+  }
+}
+</script>
+
+<style scoped lang="css">
+.centerLogo {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <Index></Index>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Index from "~/components/arsanandha/Index.vue";
+
+export default Vue.extend({
+  head:{
+    title: 'arsanandha.xyz',
+    meta: [
+      { charset: 'utf-8'},
+      { name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {
+        hid: 'arsanandha.xyz',
+        name: 'arsanandha.xyz',
+        content: 'arsanandha.xyz'
+      },
+      { property: "og:site_name", content: "arsanandha.xyz" },
+      { hid: "og:type", property: "og:type", content: "website" },
+      {
+        hid: "og:url",
+        property: "og:url",
+        content: "https://inthanon.arsanandha.xyz",
+      },
+      {
+        hid: "og:title",
+        property: "og:title",
+        content: "Inthanon (behalf of arsanandha.xyz)",
+      },
+      {
+        hid: "og:description",
+        property: "og:description",
+        content: "Inthanon is highest mountain in Thailand.",
+      },
+      {
+        hid: "og:image",
+        property: "og:image",
+        content: "https://images.unsplash.com/photo-1512553353614-82a7370096dc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80",
+      },
+      { property: "og:image:width", content: "640" },
+      { property: "og:image:height", content: "360" },
+    ]
+  },
+  components: {Index}
+})
+</script>
